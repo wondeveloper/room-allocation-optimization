2d250bcd34b6cd084006a1d1922206f3fb22c56d
Vivek Kumar <Vivek.kumar4329@gmail.com>
2026-02-02 09:44:19 +0530

    handling only premium or only economy guests

--- src/main/java/com/smarthost/allocationservice/service/RoomAllocationServiceImpl.java
+++ src/main/java/com/smarthost/allocationservice/service/RoomAllocationServiceImpl.java
@@ -25,23 +25,33 @@ public non-sealed class RoomAllocationServiceImpl implements RoomAllocationServi
     @Override
     public RoomQueryResponse allocateRooms(RoomQueryRequest request) {
         logger.debug("Allocating rooms for premium and economy client requests with premium room cut off amount: {}", premiumAmountThreshold);
-        Map<RoomType, PriorityQueue<BigDecimal>> map = request.potentialGuests().stream()
-                .collect(Collectors.groupingBy(quote -> quote.compareTo(new BigDecimal(premiumAmountThreshold)) >= 0 ? RoomType.PREMIUM : RoomType.ECONOMY,
-                        Collectors.toCollection(() ->new PriorityQueue<>(Collections.reverseOrder()))));
+        Map<RoomType, PriorityQueue<BigDecimal>> roomTypePriorityQueueMap = getRoomTypePriorityQueueMap(request);
 
         final RoomOccupancyHolder roomOccupancyHolder = RoomOccupancyHolder.of(request.premiumRooms(),request.economyRooms());
 
         //Premium room allocation
-        RoomOccupancyHolder premiuimRoomOccupancyHolder = PremiumRoomAllocator.allocatePremium(roomOccupancyHolder, map.get(RoomType.PREMIUM));
+        RoomOccupancyHolder premiuimRoomOccupancyHolder = PremiumRoomAllocator.allocatePremium(roomOccupancyHolder, roomTypePriorityQueueMap.get(RoomType.PREMIUM));
 
         //Economy rooms allocation
-        RoomOccupancyHolder economyRoomOccupancyHolder = EconomyRoomAllocator.allocateEconomy(premiuimRoomOccupancyHolder,map.get(RoomType.ECONOMY));
+        RoomOccupancyHolder economyRoomOccupancyHolder = EconomyRoomAllocator.allocateEconomy(premiuimRoomOccupancyHolder,roomTypePriorityQueueMap.get(RoomType.ECONOMY));
 
         logger.info("Smart upgrade started");
         //Smart upgrade
-        RoomOccupancyHolder premiumRoomSmartUpgradeOccupancyHolder = PremiumRoomAllocator.allocatePremium(economyRoomOccupancyHolder,map.get(RoomType.ECONOMY));
+        RoomOccupancyHolder premiumRoomSmartUpgradeOccupancyHolder = PremiumRoomAllocator.allocatePremium(economyRoomOccupancyHolder,roomTypePriorityQueueMap.get(RoomType.ECONOMY));
 
         logger.debug("Rooms allocation finished for current request");
         return QueryResponseMapper.fromRoomOccupancyHolder(request.premiumRooms(), request.economyRooms(),premiumRoomSmartUpgradeOccupancyHolder);
+    }
+
+    private Map<RoomType, PriorityQueue<BigDecimal>> getRoomTypePriorityQueueMap(RoomQueryRequest request) {
+        return request.potentialGuests().stream()
+                .collect(Collectors.groupingBy(quote -> quote.compareTo(new BigDecimal(premiumAmountThreshold)) >= 0 ? RoomType.PREMIUM : RoomType.ECONOMY,
+                        () -> {
+                            Map<RoomType, PriorityQueue<BigDecimal>> initialMap = new EnumMap<>(RoomType.class);
+                            initialMap.put(RoomType.PREMIUM, new PriorityQueue<>(Collections.reverseOrder()));
+                            initialMap.put(RoomType.ECONOMY, new PriorityQueue<>(Collections.reverseOrder()));
+                            return initialMap;
+                        },
+                        Collectors.toCollection(() ->new PriorityQueue<>(Collections.reverseOrder()))));
     }
 }
